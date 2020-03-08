baca73b2e3204727c4155b7b13866962ce5362b0
Samrith Shankar <samrith@thetotem.co>
2020-03-08 23:19:56 +0100

    Fixed queries not working correctly.

--- src/resolvers/Queries/index.ts
+++ src/resolvers/Queries/index.ts
@@ -1,1 +1,9 @@
-export { UserQueries } from './User.queries';
+import { queryType } from 'nexus';
+
+import { UserQueries } from './User.queries';
+
+export const Query = queryType({
+    definition(t) {
+        UserQueries(t);
+    },
+});
