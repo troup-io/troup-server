ecfd1666c31d709fd2216c1e3f7c4c9bd0a7f29f
Samrith Shankar <samrith@thetotem.co>
2020-03-09 03:17:39 +0100

    Converted NODE_ENV to an enum

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        NODE_ENV: string;
+        NODE_ENV: 'development' | 'simulation' | 'production';
         APP_SECRET: string;
     }
 }
