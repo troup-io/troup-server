f3eae53242652002ad4434da479c4dfc4dee0557
Samrith Shankar <samrith.298@gmail.com>
2020-04-28 19:54:59 +0200

    Changed environment option from 'simulation' to 'staging'

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        NODE_ENV: 'development' | 'simulation' | 'production';
+        NODE_ENV: 'development' | 'staging' | 'production';
         APP_SECRET: string;
     }
 }
